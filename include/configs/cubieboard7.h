6cd65dddbaa342bd070d8e479eab069fc2252be2
huanghaiyu <huanghaiyu05@163.com>
2018-01-12 16:21:14 +0800

    Basic porting for cubieboard7

---
+++ include/configs/cubieboard7.h
@@ -0,0 +1,13 @@
+/*
+ * (C) Copyright 2016 wowotech 
+ *
+ * wowowowo@wowotech.net 
+ *
+ * Configuration for cubieboard7. 
+ *
+ * SPDX-License-Identifier:    GPL-2.0+ 
+ **/ 
+#ifndef __CUBIEBOARD7_H 
+#define __CUBIEBOARD7_H 
+
+#endif
