629f589dc8ce23f87cffa975a2d1f334e8458999
huanghaiyu <huanghaiyu05@163.com>
2018-01-15 18:49:40 +0800

    [cb7]: add cubieboard7 board configs

---
+++ board/actions/cubieboard7/board.c
@@ -0,0 +1,201 @@
+/*
+ * (C) Copyright 2016 wowotech
+ *
+ * wowo<wowo@wowotech.net>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#include <common.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <asm/armv8/mmu.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define GPIOA_OUTEN	(0xe01b0000)
+#define GPIOA_OUTDAT	(0xe01b0008)
+#define DEBUG_LED0_GPIO	(19)	/* GPIOA19 */
+#define DEBUG_LED1_GPIO	(20)	/* GPIOA20 */
+
+#define GPIOB_OUTEN		(0xe01b000c)
+#define GPIOB_OUTDAT		(0xe01b0014)
+#define DEBUG_LED1_YELLOW 	(8)	/* GPIOB8 */
+#define DEBUG_LED0_BLUE		(9)	/* GPIOB9 */
+
+#define GPIOF_OUTEN	(0xe01b00f0)
+#define GPIOF_OUTDAT	(0xe01b00f8)
+#define DEBUG_LED2_GPIO	(1)	/* GPIOF1 */
+#define DEBUG_LED3_GPIO	(2)	/* GPIOF2 */
+
+static struct mm_region bubblegum_mem_map[] = {
+	{
+		.virt = 0x0UL,
+		.phys = 0x0UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0x80000000UL,
+		.phys = 0x80000000UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+		/* List terminator */
+		0,
+	}
+};
+
+static void reboot_to_adfu(void)
+{
+	void (*call_adfu)(void);
+
+	//call_adfu = (void (*)(void))0xffff5a00;
+	call_adfu = (void (*)(void))0xffff0400;
+
+	call_adfu();
+	while (1);
+}
+
+
+/*
+ * A simple debug function for early debug, in which,
+ * we use four LEDs to display sixteen debug codes, from 0 to 15.
+ * Using it, we can know, at least roughly, at where out code is run.
+ */
+void bubblegum_early_debug(int debug_code)
+{
+	uint8_t val;
+
+	val = debug_code & 0x1;
+	setbits_le32(GPIOA_OUTEN, 1 << DEBUG_LED0_GPIO);
+	clrsetbits_le32(GPIOA_OUTDAT, 1 << DEBUG_LED0_GPIO,
+			val << DEBUG_LED0_GPIO);
+
+	val = (debug_code >> 1) & 0x1;
+	setbits_le32(GPIOA_OUTEN, 1 << DEBUG_LED1_GPIO);
+	clrsetbits_le32(GPIOA_OUTDAT, 1 << DEBUG_LED1_GPIO,
+			val << DEBUG_LED1_GPIO);
+
+	val = (debug_code >> 2) & 0x1;
+	setbits_le32(GPIOF_OUTEN, 1 << DEBUG_LED2_GPIO);
+	clrsetbits_le32(GPIOF_OUTDAT, 1 << DEBUG_LED2_GPIO,
+			val << DEBUG_LED2_GPIO);
+
+	val = (debug_code >> 3) & 0x1;
+	setbits_le32(GPIOF_OUTEN, 1 << DEBUG_LED3_GPIO);
+	clrsetbits_le32(GPIOF_OUTDAT, 1 << DEBUG_LED3_GPIO,
+			val << DEBUG_LED3_GPIO);
+}
+
+struct mm_region *mem_map = bubblegum_mem_map;
+
+#ifdef CONFIG_SPL_BUILD
+void board_init_f(ulong bootflag)
+{
+	bubblegum_early_debug(1);
+
+#ifdef HAS_DDR_SOURCE_CODE
+	s900_ddr_init();
+#endif
+
+	reboot_to_adfu();
+}
+
+void board_init_r(gd_t *new_gd, ulong dest_addr)
+{
+}
+
+void panic(const char *fmt, ...)
+{
+}
+#endif
+
+int board_early_init_f(void)
+{
+	bubblegum_early_debug(1);
+	//while (1);
+
+	return 0;
+}
+
+int board_init(void)
+{
+	bubblegum_early_debug(2);
+	return 0;
+}
+
+struct serial_device *default_serial_console(void)
+{
+	return NULL;
+}
+
+void reset_cpu(ulong addr)
+{
+}
+
+extern void s900_ddr_init(void);
+int dram_init(void)
+{
+	printf("dram_init\n");
+
+	bubblegum_early_debug(11);
+
+	/* no need do dram init in here, we have done it in SPL */
+
+	gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+
+	printf("dram_init OK\n");
+	return 0;
+}
+
+#if defined(CONFIG_SYS_DRAM_TEST)
+int testdram (void)
+{
+	uint32_t *pstart = (uint32_t *)CONFIG_SYS_MEMTEST_START;
+	uint32_t *pend = (uint32_t *)CONFIG_SYS_MEMTEST_END;
+	uint32_t *p;
+
+	printf("SDRAM test phase 1:\n");
+	printf("write...\n");
+	for (p = pstart; p < pend; p++)
+		*p = 0xaaaaaaaa;
+
+	printf("read back and check...\n");
+	for (p = pstart; p < pend; p++) {
+		if (*p != 0xaaaaaaaa) {
+			printf ("SDRAM test fails at: %08x=%08x\n", (uint32_t)p, *p);
+			while (1);
+		}
+	}
+
+	printf("SDRAM test phase 2:\n");
+	printf("write...\n");
+	for (p = pstart; p < pend; p++)
+		*p = 0x55555555;
+
+	printf("read back and check...\n");
+	for (p = pstart; p < pend; p++) {
+		if (*p != 0x55555555) {
+			printf ("SDRAM test fails at: %08x=%08x\n", (uint32_t)p, *p);
+			while (1);
+		}
+	}
+
+	printf("SDRAM test passed.\n");
+	return 0;
+}
+#endif
+
+int board_run_command(const char *cmdline)
+{
+	printf("## Commands are disabled. Please enable CONFIG_CMDLINE.\n");
+
+	return 1;
+}
+
+int print_cpuinfo(void)
+{
+	return 0;
+}
--- include/configs/cubieboard7.h
+++ include/configs/cubieboard7.h
@@ -10,4 +10,73 @@
 #ifndef __CUBIEBOARD7_H 
 #define __CUBIEBOARD7_H 
 
+#define CONFIG_SPL_TEXT_BASE 0xe4063200
+#define CONFIG_SPL_MAX_SIZE (1024 * 20)
+
+#define CONFIG_SPL_BSS_START_ADDR	(CONFIG_SPL_TEXT_BASE + \
+					 CONFIG_SPL_MAX_SIZE)
+#define CONFIG_SPL_BSS_MAX_SIZE		(1024 * 16)
+
+#define CONFIG_SPL_STACK 0xe4071000
+
+
+/*
+ *  * SDRAM Definitions
+ *   */
+
+#define CONFIG_SYS_SDRAM_BASE		0x0
+#define CONFIG_NR_DRAM_BANKS		1
+
+#define CONFIG_SYS_SDRAM_SIZE		0x80000000	/* 2GB */
+
+/*
+ *  * u-boot definitions, which is resided in SDRAM, TODO
+ *   */
+
+#define CONFIG_SYS_TEXT_BASE		0x11000000
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_SDRAM_BASE + 0x7ff00)
+
+/* Some commands use this as the default load address, TODO */
+#define CONFIG_SYS_LOAD_ADDR		(CONFIG_SYS_SDRAM_BASE + 0x7ffc0)
+
+/* for DRAM test */
+/* #define CONFIG_SYS_DRAM_TEST */
+#define CONFIG_SYS_MEMTEST_START	CONFIG_SYS_INIT_SP_ADDR
+#define CONFIG_SYS_MEMTEST_END		CONFIG_SYS_TEXT_BASE
+
+/* Generic Interrupt Controller Definitions */
+#define CONFIG_GICV2
+
+//#define GICD_BASE			(0xe00f1000)
+//#define GICC_BASE			(0xe00f2000)
+
+/* Do not preserve environment */
+#define CONFIG_ENV_IS_NOWHERE		1
+#define CONFIG_ENV_SIZE			0x1000	/* 4K Bytes */
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		(16 * 1024 + CONFIG_ENV_SIZE)
+
+#define CONFIG_SYS_CBSIZE		512	/* Console I/O Buffer Size */
+#define CONFIG_SYS_MAXARGS		16	/* max command args */
+
+/* UART Definitions */
+#define CONFIG_BAUDRATE			115200
+
+/* FLASH and environment organization */
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_BOARD_EARLY_INIT_F
+
+#define CONFIG_BOOTARGS                 "earlycon=owl_serial "\
+					"console=ttyS5 " \
+					"root=/dev/ram0 " \
+					"init=/sbin/init " \
+					"loglevel=8"
+
+#define CONFIG_BOOTCOMMAND		"bootm 0x6400000"
+
+#define COUNTER_FREQUENCY		(24000000)      /* 24MHz */
+
+
 #endif
